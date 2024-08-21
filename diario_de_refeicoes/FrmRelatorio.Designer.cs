26b22dd36d56f6858ed9c3447aac540753771f82
nevesjp <jamersonp53@gmail.com>
2024-08-21 17:21:36 -0300

    - no form de relatorio, foi incluido os campos de data fina e inical, bem como o grid caregando os resultados do banco. - validacao para evitar fazer a busca com a data vazia -

--- diario_de_refeicoes/FrmRelatorio.Designer.cs
+++ diario_de_refeicoes/FrmRelatorio.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmRelatorio";
+            mskDataInicial = new MaskedTextBox();
+            mskDataFinal = new MaskedTextBox();
+            btnFiltrar = new Button();
+            grdResult = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)grdResult).BeginInit();
+            SuspendLayout();
+            // 
+            // mskDataInicial
+            // 
+            mskDataInicial.Location = new Point(26, 23);
+            mskDataInicial.Mask = "00/00/0000";
+            mskDataInicial.Name = "mskDataInicial";
+            mskDataInicial.Size = new Size(100, 23);
+            mskDataInicial.TabIndex = 1;
+            mskDataInicial.ValidatingType = typeof(DateTime);
+            // 
+            // mskDataFinal
+            // 
+            mskDataFinal.Location = new Point(208, 23);
+            mskDataFinal.Mask = "00/00/0000";
+            mskDataFinal.Name = "mskDataFinal";
+            mskDataFinal.Size = new Size(100, 23);
+            mskDataFinal.TabIndex = 2;
+            mskDataFinal.ValidatingType = typeof(DateTime);
+            // 
+            // btnFiltrar
+            // 
+            btnFiltrar.Location = new Point(640, 25);
+            btnFiltrar.Name = "btnFiltrar";
+            btnFiltrar.Size = new Size(75, 23);
+            btnFiltrar.TabIndex = 4;
+            btnFiltrar.Text = "Filtrar";
+            btnFiltrar.UseVisualStyleBackColor = true;
+            btnFiltrar.Click += btnFiltrar_Click;
+            // 
+            // grdResult
+            // 
+            grdResult.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            grdResult.Location = new Point(12, 54);
+            grdResult.Name = "grdResult";
+            grdResult.ReadOnly = true;
+            grdResult.RowTemplate.Height = 25;
+            grdResult.Size = new Size(703, 359);
+            grdResult.TabIndex = 5;
+            // 
+            // FrmRelatorio
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(728, 425);
+            Controls.Add(grdResult);
+            Controls.Add(btnFiltrar);
+            Controls.Add(mskDataFinal);
+            Controls.Add(mskDataInicial);
+            Name = "FrmRelatorio";
+            Text = "FrmRelatorio";
+            ((System.ComponentModel.ISupportInitialize)grdResult).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MaskedTextBox mskDataInicial;
+        private MaskedTextBox mskDataFinal;
+        private Button btnFiltrar;
+        private DataGridView grdResult;
     }
 }
--- diario_de_refeicoes/FrmRelatorio.cs
+++ diario_de_refeicoes/FrmRelatorio.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,60 @@ namespace diario_de_refeicoes
 {
     public partial class FrmRelatorio : Form
     {
+        string StringConexao = "Data Source=localhost\\sqlexpress;Initial Catalog=minhas_refeicoes;Integrated Security=True;User ID=sa;Password=sysdb;Encrypt=False";
         public FrmRelatorio()
         {
             InitializeComponent();
+            loadFrm();
         }
+
+        private void loadFrm()
+        {
+            mskDataInicial.Text = DateTime.Now.ToString();
+            mskDataFinal.Text = DateTime.Now.ToString();
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            if ((mskDataInicial.Text.Trim() == string.Empty || mskDataInicial.Text == mskDataInicial.Mask || mskDataInicial.Text == "  /  /") ||
+                    (mskDataFinal.Text.Trim() == string.Empty || mskDataFinal.Text == mskDataInicial.Mask || mskDataFinal.Text == "  /  /"))
+            {
+                MessageBox.Show("O campo não pode estar vazio.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            CarregaRefeicoes();
+        }
+
+        private void CarregaRefeicoes()
+        {
+            // Converte as datas das máscaras para DateTime com as horas ajustadas
+            DateTime dataInicial = DateTime.Parse(mskDataInicial.Text).Date;
+            DateTime dataFinal = DateTime.Parse(mskDataFinal.Text).Date.AddHours(23).AddMinutes(59);
+
+            string queryString = "SELECT Nome, DataRealizado, Realizado FROM RegistroRefeicoes " +
+                                 "WHERE DataRealizado BETWEEN @DataInicial AND @DataFinal";
+
+            using (SqlConnection conexao = new SqlConnection(StringConexao))
+            {
+                SqlCommand command = new SqlCommand(queryString, conexao);
+                command.Parameters.AddWithValue("@DataInicial", dataInicial);
+                command.Parameters.AddWithValue("@DataFinal", dataFinal);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                // Atribui o DataTable como fonte de dados para o DataGridView
+                grdResult.DataSource = table;
+
+                // Se necessário, atualiza a exibição do DataGridView
+                grdResult.Refresh();
+            }
+        }
+
+
+
+
     }
 }
